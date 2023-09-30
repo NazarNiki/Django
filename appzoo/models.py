104949c3edfa3fe8b6124ba8611251edf754a332
goshva <sha.egor@gmail.com>
2023-09-30 07:49:47 +0300

    re name app

--- appzoo/models.py
+++ appzoo/models.py
@@ -7,6 +7,10 @@ class Todo_task(models.Model):
     title = models.CharField(max_length = 100)
     complete = models.BooleanField(default=False)
     created = models.DateField(auto_now_add = True)
-
+    name = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='characters')
+    created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.title
